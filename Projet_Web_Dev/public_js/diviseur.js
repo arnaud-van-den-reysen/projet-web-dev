27d623f0c5744012802a1f9ec7587ce314c29714
Arnaud VAN DEN REYSEN <vandenreysen.arnaud@gmail.com>
2019-06-25 16:14:57 +0200

    menu.css a été renommé

--- Projet_Web_Dev/public_js/diviseur.js
+++ Projet_Web_Dev/public_js/diviseur.js
@@ -3,6 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/**
+ * @author arnaud VAN DEN REYSEN
+ * 
+ */
 function calculDiviseur(){
     var vout = 0;
     
